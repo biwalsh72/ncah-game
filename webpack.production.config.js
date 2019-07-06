b9ab3fcee168a1931f518edafc943637f44bc0e0
biwalsh72 <biwalsh72@gmail.com>
2019-07-05 21:28:44 -0400

    updated production webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,65 +1,73 @@
-`/* eslint-disable import/no-extraneous-dependencies /
-/ eslint-disable arrow-body-style /
-/ eslint-disable no-unused-vars */
-
-const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
 
-const bundleExtractPlugin = new ExtractTextPlugin({
-filename: 'css/bundle.css',
-});
+const PATHS = {
+  app: {
+    index: path.resolve(__dirname, 'app', 'index.js'),
+    room: path.resolve(__dirname, 'app', 'room.js')
+  },
+  build: path.resolve(__dirname, 'public', 'build'),
+  node: path.resolve(__dirname, 'node_modules'),
+};
 
-const vendorsExtractPlugin = new ExtractTextPlugin({
-filename: 'css/vendors.css',
-});
+const config = {
+  devtool: 'source-map',
+  entry: {
+    index: [
+      PATHS.app.index
+    ],
+    room: [
+      PATHS.app.room
+    ]
+  },
 
-module.exports = (env) => {
-return {
-name: 'client',
-target: 'web',
-entry: ['./src/client/app.jsx'],
-output: {
-path: path.resolve(__dirname, 'public'),
-filename: 'js/bundle.js',
-},
-module: {
-loaders: [
-{
-test: [/.js$|.jsx$/],
-exclude: [/node_modules/],
-loader: 'babel-loader',
-options: {
-plugins: ['transform-runtime'],
-presets: ['es2015', 'stage-0', 'react'],
-},
-},
-{
-test: /.scss$/,
-exclude: [/node_modules/],
-use: bundleExtractPlugin.extract({
-use: ['css-loader', 'sass-loader'],
-}),
-},
-{
-test: /.css$/,
-exclude: [/node_modules/],
-use: vendorsExtractPlugin.extract({
-use: ['css-loader'],
-}),
-},
-],
-},
-stats: {
-colors: true,
-},
-devtool: 'source-map',
+  output: {
+    path: PATHS.build,
+    filename: '[name].bundle.js',
+    publicPath: 'http://localhost:8080/build/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            sourceMap: true
+          }
+        }
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader', options: { sourceMap: true } },
+          { loader: 'css-loader', options: { sourceMap: true, modules: 'global', } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true, modules: true } }
+        ]
+      }
+      
+      ///...
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+
+
 plugins: [
-new webpack.DefinePlugin({
-SOCKET_URL: JSON.stringify(process.env.SOCKET_URL ? process.env.SOCKET_URL : 'wss://localhost:3000'),
-}),
-bundleExtractPlugin,
-vendorsExtractPlugin,
-],
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+
+  performance: {
+    maxEntrypointSize: 2048000,
+    maxAssetSize: 2048000
+  }
 };
-};
+
+module.exports = config;
